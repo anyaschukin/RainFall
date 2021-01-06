8779ff9cabcf67204c4d75bd1883fba83f5e8faf
Drew Finnis <drewfinnis@gmail.com>
2021-01-06 17:09:58 +0100

    refacto/normed

--- level0/source.c
+++ level0/source.c
@@ -1,18 +1,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#define _GNU_SOURCE
 #include <unistd.h>
+#define _GNU_SOURCE
 
-int			main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	int password = atoi(av[1]);
+	int		password;
+	char	*args[2];
+
+	password = atoi(av[1]);
 	if (password == 423)
 	{
-		char *args[2];
 		args[0] = strdup("/bin/sh");
 		args[1] = NULL;
-
 		setresgid(getegid(), getegid(), getegid());
 		setresuid(geteuid(), geteuid(), geteuid());
 		execv("/bin/sh", args);
