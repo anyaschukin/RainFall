8c4567c09123c12f47eca0765ee8b6932a75a239
Drew Finnis <drewfinnis@gmail.com>
2021-01-13 22:53:51 +0100

    refacto/norm level3 source

--- level3/source.c
+++ level3/source.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int m;
+int	g_m;
 
-void v(void) {
-	char buffer[512];
+void	v(void)
+{
+	char	buffer[512];
 
 	fgets(buffer, 512, stdin);
 	printf(buffer);
-	if (m == 64) {
+	if (g_m == 64)
+	{
 		fwrite("Wait what?!\n", 1, 12, stdout);
 		system("/bin/sh");
-		}
-		return;
+	}
+	return ;
 }
 
-int main(void) {
+int	main(void)
+{
 	v();
-	return 0;
+	return (0);
 }
