e112022e43722f916c049c01864c283276e87bf6
Anya Schukin <anya.schukin@gmail.com>
2021-01-20 14:51:20 +0100

    fix/source.c for level7 works

--- level7/source.c
+++ level7/source.c
@@ -1,33 +1,34 @@
-#include <stdlib.h> // malloc
-#include <stdio.h> // printf, fopen, fgets, puts
-#include <string.h> // strcpy
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-char	g_c[68];
+char c[68];
 
 void	m(void)
 {
-	printf("%s - %d\n", g_c, time(0));
-	return ;
+	printf("%s - %d\n", c, (int)time(0));
 }
 
-int	main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-	char	*s1;
-	char	*s2;
+	int		*s1;
+	int		*s2;
 
-	s1 = (char *)malloc(8);
-	s1[0] = 1;
-	s1[1] = (int)malloc(8);
+	s1 = malloc(8);
+	s1[0] =1;
+	s1[1] = malloc(8);
 
-	s2 = (char *)malloc(8);
+	s2 = malloc(8);
 	s2[0] = 2;
-	s2[1] = (int)malloc(8);
+	s2[1] = malloc(8);
 
-	strcpy(s1[1], argv[1]);
-	strcpy(s2[1], argv[2]);
+	strcpy((char *)s1[1], argv[1]);
+	strcpy((char *)s2[1], argv[2]);
 
-	fgets(g_c, 68, fopen("/home/user/level8/.pass", "r"));
+	fgets(c, 68, fopen("/home/user/level8/.pass", "r"));
+
 	puts("~~");
+
 	return (0);
 }
