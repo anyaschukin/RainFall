add912f6fc86817fb1494ca0f36d7471f507dcef
Anya Schukin <anya.schukin@gmail.com>
2021-01-24 14:49:17 +0100

    refacto/fix formatting

--- level9/source.cpp
+++ level9/source.cpp
@@ -2,31 +2,26 @@
 #include <cstring>
 
 class N {
-
 	public:
-
 		char annotation[100];
 		int nb;
 
 		N(int n) {
 			nb = n;
 		}
-
 		void setAnnotation(char *str) {
 			memcpy(annotation, str, strlen(str));
 		}
-
 		virtual int operator+(N &n) {
 			return (nb + n.nb);
 		}
-
 		virtual int operator-(N &n) {
 			return (nb - n.nb);
 		}
 };
 
-int main(int ac, char **av) {
-	if (ac <= 1)
+int main(int argc, char **argv) {
+	if (argc <= 1)
 		exit(1);
 
 	N *n1 = new N(5);
@@ -35,6 +30,6 @@ int main(int ac, char **av) {
 	N *n_ptr1 = n1;
 	N *n_ptr2 = n2;
 
-	n_ptr1->setAnnotation(av[1]);
+	n_ptr1->setAnnotation(argv[1]);
 	return (*n_ptr2 + *n_ptr1);
 }
